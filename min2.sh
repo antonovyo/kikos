c89c30ea4c8478e5df33440ee183a223b45d9075
antonovyo <95147690+antonovyo@users.noreply.github.com>
2024-02-06 03:24:46 +0200

    Update and rename min.sh to min2.sh

--- min2.sh
+++ min2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    ./gram-mining-pool --address UQBu3Mi0YgcWx1zFTkRGLt1SgQ9T3dXF7JWTiFEu-_20-ivZ --gpu-auto --miner open-cl --cpu-auto
+    gram-mining-pool --address UQBu3Mi0YgcWx1zFTkRGLt1SgQ9T3dXF7JWTiFEu-_20-ivZ --gpu-auto --miner open-cl --cpu-auto
     if [ $? -ne 0 ]; then
         echo "Process exited with an error. Restarting..."
     else
