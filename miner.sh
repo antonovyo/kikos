4ca6ca6d613024f442e1a702f6dce631a5ad82e8
antonovyo <95147690+antonovyo@users.noreply.github.com>
2024-02-06 03:02:13 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
 while true; do
     ./gram-mining-pool --address UQBu3Mi0YgcWx1zFTkRGLt1SgQ9T3dXF7JWTiFEu-_20-ivZ --gpu-auto --miner open-cl
-
-    # Check the exit status of the last command
     if [ $? -eq 0 ]; then
-        # If the command exited successfully, break the loop
         break
     else
-        # If the command failed, wait for a while (optional) and then restart
-        sleep 5  # You can adjust the sleep duration as needed
+        sleep 5
     fi
 done
