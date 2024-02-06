3c785dabc5ae72a88ea8a0da053dd127f600a444
antonovyo <95147690+antonovyo@users.noreply.github.com>
2024-02-06 03:24:06 +0200

    Update and rename miner.sh to min.sh

--- miner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-    ./gram-mining-pool --address UQBu3Mi0YgcWx1zFTkRGLt1SgQ9T3dXF7JWTiFEu-_20-ivZ --gpu-auto --miner open-cl
-    sleep 5  # Optional: add a delay before restarting to avoid constant restarts in case of immediate failure
-done
